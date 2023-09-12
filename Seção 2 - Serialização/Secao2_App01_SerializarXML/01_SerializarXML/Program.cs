4afaec200e2d683ea3f564ce38e4a73990314dcd
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-09-12 00:30:01 -0300

    Início seção 2

---
+++ Seção 2 - Serialização/Secao2_App01_SerializarXML/01_SerializarXML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using _00_Biblioteca;
+using System.Xml.Serialization;
+
+namespace _01_SerializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "Rafael Santos da Siltva" , Cpf="123.456.789-22", Email="teste@email.com"};
+
+
+            //XmlSerializer serializadorXml = new XmlSerializer(usuario.GetType());
+
+            XmlSerializer serializadorXml = new XmlSerializer(typeof(Usuario));
+
+            StreamWriter stream = new StreamWriter(@"C:\Users\RAFAEL\OneDrive\Documentos\Arquivo - C# Avançado\serializarXML.xml");
+
+            serializadorXml.Serialize(stream , usuario);
+        }
+    }
+}
