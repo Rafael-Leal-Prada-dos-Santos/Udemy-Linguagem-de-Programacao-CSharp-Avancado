b8f850cb3c1a2431844ef2528a7da9571e33831f
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-09-16 17:56:15 -0300

    Até aula 9

--- Seção 3 - Tópicos Avançados/02 - Topicos Avancados/01_Generics/Serializador.cs
+++ Seção 3 - Tópicos Avançados/02 - Topicos Avancados/01_Generics/Serializador.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace _01_Generics
 {
-    public class Serializador
+    public class Serializador 
     {
-        public static void Serializar(obj)
+        //public static void Serializar<T>(T objeto) 
+        //    where T: class
+        //{
+
+        //}
+
+        public static void Serializar(object objeto)
         {
+            //StreamWriter sw = new StreamWriter($@"C:\Users\RAFAEL\OneDrive\Documentos\Arquivo - C# Avançado\03_(NomeDaClasse).txt");
+            StreamWriter sw = new StreamWriter($@"C:\Users\RAFAEL\OneDrive\Documentos\Arquivo - C# Avançado\03_{objeto.GetType().Name}.txt");
+
+            string json = JsonConvert.SerializeObject(objeto);
+
+            sw.Write(json);
+            sw.Close();
+
+            Console.WriteLine("Método de serializar executado com sucesso!");
         }
 
-        public static (Classe) Desserializar() { }
+        public static T Desserializar<T>()
+            where T : class
+        {
+            StreamReader sr = new StreamReader($@"C:\Users\RAFAEL\OneDrive\Documentos\Arquivo - C# Avançado\03_{nameof(T)}.txt");
+
+            return null;
+        }
     }
 }
