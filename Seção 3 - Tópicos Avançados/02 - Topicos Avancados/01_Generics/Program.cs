93f0acdde9f24826b82965896799a2521a276f1b
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-09-16 16:09:28 -0300

    Aula 8

---
+++ Seção 3 - Tópicos Avançados/02 - Topicos Avancados/01_Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using _01_Generics.Modelo;
+
+namespace _01_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro()
+            {
+                Marca = "FIAT",
+                Modelo = "UNO"
+            };
+
+            Casa casa = new Casa()
+            {
+                Cidade = "Brasilia",
+                Endereco = "Amador blueno 393"
+            };
+
+            Usuario usuario = new Usuario() 
+            {
+                Nome = "MAria",
+                Email = "maria@gmail.com",
+                Senha = "123456"
+            };
+
+            Serializador.Serializar(carro);
+            Serializador.Serializar(casa);
+            Serializador.Serializar(usuario);
+
+            Carro carro2 = Serializador.Desserializar();
+        }
+    }
+}
