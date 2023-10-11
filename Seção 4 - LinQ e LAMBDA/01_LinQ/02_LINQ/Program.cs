1c3f658bcebf6a1462d93704310f7251911072af
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-10-11 13:51:16 -0300

    Seção 4 Completa

---
+++ Seção 4 - LinQ e LAMBDA/01_LinQ/02_LINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lista = { 3, 9, 4, 6, 20, 10, 60, 90, 80, 50 };
+
+            //(Filtro)Pesquisar valores na lista que atendam uma determinada condição
+            lista.Where(a => a > 10);
+
+            //Selecionar os dados que quer apresentar
+            lista.Select(a => a);
+
+            Console.WriteLine("Imprimindo a lista filtrada pela forma literal");
+
+            //IEnumerable<int> ListaFiltrada = lista.Where(a => a > 10).Select(a => a);
+
+            //Forma literal
+            IOrderedEnumerable<int> ListaFiltrada = from a in lista where a > 10 orderby a select a;
+
+            foreach (int item in ListaFiltrada)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Seção 4 - LinQ e LAMBDA/01_LinQ/03_LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            lista.Add(new Usuario() { Nome = "rafael", Email = "rafael@gmail.com" });
+            lista.Add(new Usuario() { Nome = "richard", Email = "richard@hotmail.com" });
+            lista.Add(new Usuario() { Nome = "renato", Email = "renato@ig.com" });
+            lista.Add(new Usuario() { Nome = "jeferson", Email = "jeferson@hotmail.com" });
+            lista.Add(new Usuario() { Nome = "pedro", Email = "pedro@gmail.com" });
+
+            var listaFiltrada = lista.Where(a => a.Email.Contains("@gmail.com")).Select(a => a);
+
+            foreach (var item in listaFiltrada) 
+            {
+                Console.WriteLine(item.Nome);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
