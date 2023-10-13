a6acd805c7f22da229bdcef25e4a409c764572d9
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-10-12 23:41:30 -0300

    Início seção delegates

---
+++ Seção 5 - Delegate e Eventos/DelegateEEventos/02_Delegates/Lib/FotoFiltro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Delegates.Lib
+{
+    public class FotoFiltro
+    {
+        public void Colorir(Foto foto) 
+        {
+            //Algoritimo
+            Console.WriteLine("FotoFiltro > Colorir");
+        }
+
+        public void GerarThumb(Foto foto)
+        {
+            //Algoritimo
+            Console.WriteLine("FotoFiltro > GerarThumb");
+        }
+
+        public void PretoBranco(Foto foto)
+        {
+            //Algoritimo
+            Console.WriteLine("FotoFiltro > PretoBranco");
+        }
+
+        public void RedimensionarTamanhoMedio(Foto foto)
+        {
+            //Algoritimo
+            Console.WriteLine("FotoFiltro > RedimensionarTamanhoMedio");
+        }
+    }
+}
---
+++ Seção 5 - Delegate e Eventos/DelegateEEventos/02_Delegates/Lib/FotoProcessador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Delegates.Lib
+{
+    public class FotoProcessador
+    {
+        public static void Processador(Foto foto) 
+        {
+            FotoFiltro filtros = new FotoFiltro();
+
+            filtros.Colorir(foto);
+            filtros.GerarThumb(foto);
+            filtros.PretoBranco(foto);
+            filtros.RedimensionarTamanhoMedio(foto);
+
+        }
+    }
+}
