b221a37d65beb2ce05613c54497776649c78172c
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-10-16 13:39:26 -0300

    Final Seção 5

---
+++ Seção 5 - Delegate e Eventos/DelegateEEventos/04_DelegateMethod/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace _04_DelegateMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnClick
+            // 
+            this.BtnClick.Location = new System.Drawing.Point(55, 29);
+            this.BtnClick.Name = "BtnClick";
+            this.BtnClick.Size = new System.Drawing.Size(111, 41);
+            this.BtnClick.TabIndex = 0;
+            this.BtnClick.Text = "Clique";
+            this.BtnClick.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 106);
+            this.Controls.Add(this.BtnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnClick;
+    }
+}
+
---
+++ Seção 5 - Delegate e Eventos/DelegateEEventos/04_DelegateMethod/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_DelegateMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //BtnClick.Click += delegate (object sender, EventArgs e)
+            //{
+            //    MessageBox.Show("Olá, fui clicado!");
+            //};
+
+            BtnClick.Click += delegate
+            {
+                MessageBox.Show("Olá, fui clicado!");
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
