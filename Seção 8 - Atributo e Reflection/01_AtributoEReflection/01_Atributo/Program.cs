a80dd3012a38958c7be60d38203d0cea1212b3b3
Rafael Santos <rpsantos@iportsolutions.com.br>
2024-01-09 13:58:51 -0300

    Commit inicio sessao 8

---
+++ Seção 8 - Atributo e Reflection/01_AtributoEReflection/01_Atributo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Atributo
+{
+    class Program
+    {
+        /* Atributos são Metadados, ou seja, um dado que serve 
+         para explicar vários dados*/
+
+        [MeuAtributo("Atributo Classe", Descricao ="Descrição do Atributo")]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
